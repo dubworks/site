892384c1451005bbb5450004eebdb0e5714393fb
EC2 Default User <ec2-user@ip-172-31-0-155.ec2.internal>
2017-07-24 13:04:30 +0000

    init

---
+++ js/typer.js
@@ -0,0 +1,10 @@
+// Can also be included with a regular script tag
+
+$(function(){
+    $("#typed").typed({
+      strings: ["coworking space", "workshops", "innovation", "collaboration", "incubation"],
+      typeSpeed: 0,
+      backDelay: 1000,
+      loop: false,
+    });
+});
